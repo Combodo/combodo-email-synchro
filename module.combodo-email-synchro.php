e026fb36eeca046d19c6add49fd9d977bd8e0c25
Eric Espié <eric.espie@combodo.com>
2018-02-23 11:10:06 +0000

    N°478 - Customizable access to the 'Admin Tools'
    SVN:trunk[6524]

--- module.combodo-email-synchro.php
+++ module.combodo-email-synchro.php
@@ -2,7 +2,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-email-synchro/3.0.8',
+	'combodo-email-synchro/3.0.9',
     array(
         // Identification
         'label' => 'Tickets synchronization via e-mail',
