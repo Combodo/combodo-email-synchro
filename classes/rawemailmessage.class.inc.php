d22ca34df7994ea3b4f8b20b17d040798f845cbe
jbostoen <->
2020-12-23 15:44:37 +0100

    Version 2020-12-23 15:44:37
    * changed original regex to @Hipska's preferred solution

--- classes/rawemailmessage.class.inc.php
+++ classes/rawemailmessage.class.inc.php
@@ -912,7 +912,7 @@ class RawEmailMessage
 		// The client was Windows Live Mail, but it seems the email address was somehow misconfigured while still able to send emails.
 		// This fix still processes invalid From: headers where there are spaces before or after the email address.
 		// This regex is quite strict to prevent other issues from occurring because of this fix.
-		if(preg_match('/^(.*)<\s*([^ ]+@[^ ]+\.[^ ]+)\s*>$/', $sAddress, $aMatches))
+		if(preg_match('/^(.*)<\s*(.+?)\s*>$/', $sAddress, $aMatches))
 		{
 			$sName = trim($aMatches[1], ' "');
 			$sEmail = $aMatches[2];
