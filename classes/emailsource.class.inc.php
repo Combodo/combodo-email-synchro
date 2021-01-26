b1f9814572a3854fe6caf31d7ffc3f393fd677d3
jbostoen <->
2021-01-26 12:38:49 +0100

    Version 2021-01-26 12:38:49
    * updated based on feedback. Added method on generic EmailSource class

--- classes/emailsource.class.inc.php
+++ classes/emailsource.class.inc.php
@@ -48,6 +48,13 @@ abstract class EmailSource
 	abstract public function GetMessage($index);
 
 	/**
+	 * Initializes the message when it is being processed.
+	 * @param $index integer The index between zero and count
+	 * @return void
+	 */
+	abstract public function InitMessage($index);
+	
+	/**
 	 * Deletes the message of the given index [0..Count] from the mailbox
 	 * @param $index integer The index between zero and count
 	 */
