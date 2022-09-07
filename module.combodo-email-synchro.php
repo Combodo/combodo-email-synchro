73ead6f068305ae48c76952760a6c1bdde2ea583
acognet <anne-catherine.cognet@combodo.com>
2022-09-07 11:21:56 +0200

    Change to 3.6.1-dev

--- module.combodo-email-synchro.php
+++ module.combodo-email-synchro.php
@@ -2,17 +2,17 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'combodo-email-synchro/3.6.0',
-    array(
-        // Identification
-        'label' => 'Tickets synchronization via e-mail',
-        'category' => 'business',
-        // Setup
-        'dependencies' => array(//			'itop-config-mgmt/3.0.0', // iTop 3.0.0 minimum is not necessary for now, maybe in the future ?
-        ),
-        'mandatory' => false,
-        'visible' => true,
-		'installer' => 'EmailSynchroInstaller',
+	'combodo-email-synchro/3.6.1-dev',
+	array(
+		// Identification
+		'label'        => 'Tickets synchronization via e-mail',
+		'category'     => 'business',
+		// Setup
+		'dependencies' => array(//			'itop-config-mgmt/3.0.0', // iTop 3.0.0 minimum is not necessary for now, maybe in the future ?
+		),
+		'mandatory'    => false,
+		'visible'      => true,
+		'installer'    => 'EmailSynchroInstaller',
 		// Components
 		'datamodel' => array(
 			'classes/autoload.php',
