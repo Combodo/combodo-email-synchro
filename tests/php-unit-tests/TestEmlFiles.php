cf327194a151f098c3eb7d6fa1350e86afc0d0fc
Pierre Goiffon <pierre.goiffon@combodo.com>
2023-09-07 11:44:22 +0200

    Fix hardcoded path in TestEmlFiles

--- tests/php-unit-tests/TestEmlFiles.php
+++ tests/php-unit-tests/TestEmlFiles.php
@@ -24,7 +24,7 @@ class TestEmlFiles extends ItopTestCase
 	{
 		parent::setUp();
 
-		require_once(APPROOT.'env-production/combodo-email-synchro/classes/rawemailmessage.class.inc.php');
+		$this->RequireOnceItopFile('env-production/combodo-email-synchro/classes/rawemailmessage.class.inc.php');
 	}
 
 	/**
@@ -107,7 +107,7 @@ class TestEmlFiles extends ItopTestCase
 	{
 		parent::setUp();
 
-		$aFiles = glob(APPROOT . 'env-production/combodo-email-synchro/test/emailsSample/*.eml');
+		$aFiles = glob(__DIR__ . '/test/emailsSample/*.eml');
 
 		$aMetaData = array(
 			'email_042.eml' => array(
@@ -138,7 +138,7 @@ class TestEmlFiles extends ItopTestCase
 	 */
 	public function testMultilineLongSubjects($sEmailFilename, $sSubjectExpectedValue): void
 	{
-		$sEmlFilePath = APPROOT.'env-production/combodo-email-synchro/test/emailsSample/'.$sEmailFilename;
+		$sEmlFilePath = __DIR__ . '/test/emailsSample/' . $sEmailFilename;
 		$this->assertFileExists($sEmlFilePath, 'EML file is not existing');
 
 		$oEmail = RawEmailMessage::FromFile($sEmlFilePath);
