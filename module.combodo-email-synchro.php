38ca552c9f6772c56f56c3efcd54bad6bdf9bc20
acognet <anne-catherine.cognet@combodo.com>
2023-10-05 17:01:37 +0200

    Change module version to 3.7.5-dev

--- module.combodo-email-synchro.php
+++ module.combodo-email-synchro.php
@@ -2,7 +2,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-email-synchro/3.7.4',
+	'combodo-email-synchro/3.7.5-dev',
 	array(
 		// Identification
 		'label'        => 'Tickets synchronization via e-mail',
