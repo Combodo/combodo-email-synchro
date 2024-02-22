69576ad87e6ff6db81da9c888f6105c2f674acc5
Pierre Goiffon <pierre.goiffon@combodo.com>
2024-02-22 11:18:27 +0100

    N°7282 decoding-test.php : add details param If not present won't output body and parts

--- utils/decoding-test.php
+++ utils/decoding-test.php
@@ -8,9 +8,9 @@
  * Stand-alone command-line tool to test the decoding of a message (as a .eml file)
  *
  * Usage: php decoding-test.php <eml.file>
- * Usage: php decoding-test.php <eml.file>
+ * Usage: php decoding-test.php <eml.file> details : to output body contents
  *
- * @since 3.7.7 
+ * @since 3.7.7
  */
 
 use Laminas\Mail\Message;
@@ -24,7 +24,8 @@ date_default_timezone_set('Europe/Paris');
 function Usage()
 {
 	echo "Test the decoding of an email message (stored as a .eml file)\n";
-	echo "Usage: php decoding-test.php <eml.file>\n\n";
+	echo "Usage: php decoding-test.php <eml.file>\n";
+	echo "Usage: php decoding-test.php <eml.file> details\n\n";
 }
 
 if ($argc != 2)
@@ -36,6 +37,7 @@ if ($argc != 2)
 ///////////////////////////////////////////////////////////////
 
 $sFilePath = $argv[1];
+$bOutputDetails = (isset($argv[2]) && ($argv[2] === 'details'));
 $sPartsOrder = 'text/html,text/plain,application/pkcs7-mime';
 
 echo "# Decoding test for file '$sFilePath'\n";
@@ -55,7 +57,12 @@ try {
 	echo "------ HEADER\n";
 	var_export($oLaminasMessage->getHeaders()->toArray(), false);
 	echo "------ BODY\n";
-	var_export($oLaminasMessage->getBodyText(), false);
+	if ($bOutputDetails) {
+		var_export($oLaminasMessage->getBodyText(), false);
+	} else {
+		$sLaminasBodyLength = mb_strlen($oLaminasMessage->getBodyText());
+		echo "length: $sLaminasBodyLength\n";
+	}
 } catch (Exception $e) {
 	echo "💥 An exception was returned :(\n";
 	var_export([
@@ -106,51 +113,54 @@ echo "\n\n\n\n\n\n";
 echo "========================================================================================================================\n";
 echo "------ BODY\n";
 echo "Body Format: {$oEmail->sBodyFormat}\n\n";
-echo "Body Text:\n{$oEmail->sBodyText}\n\n";
-if ($oEmail->sBodyFormat == 'text/html') {
-	if (preg_match_all('/<img[^>]+src=(?:"cid:([^"]+)"|cid:([^ >]+))[^>]*>/i', $oEmail->sBodyText, $aMatches, PREG_OFFSET_CAPTURE)) {
-		//print_r($aMatches);
-		$aInlineImages = array();
-		foreach ($aMatches[0] as $idx => $aInfo) {
-			$aInlineImages[$idx] = array('position' => $aInfo[1]);
-		}
-		foreach ($aMatches[1] as $idx => $aInfo) {
-			$sCID = $aInfo[0];
-			if (!array_key_exists($sCID, $aCIDToImage)) {
-				echo "ERROR: inline image: $sCID NOT FOUND !!!\n";
-			} else {
-				$aInlineImages[$idx]['cid'] = $sCID;
-				echo "Ok, inline image {$aCIDToImage[$sCID]['filename']} as cid:$sCID\n";
+if ($bOutputDetails) {
+	echo "Body Text:\n{$oEmail->sBodyText}\n\n";
+	if ($oEmail->sBodyFormat == 'text/html') {
+		if (preg_match_all('/<img[^>]+src=(?:"cid:([^"]+)"|cid:([^ >]+))[^>]*>/i', $oEmail->sBodyText, $aMatches, PREG_OFFSET_CAPTURE)) {
+			//print_r($aMatches);
+			$aInlineImages = array();
+			foreach ($aMatches[0] as $idx => $aInfo) {
+				$aInlineImages[$idx] = array('position' => $aInfo[1]);
 			}
-		}
-		$sWholeText = $oEmail->sBodyText;
-		$idx = count($aInlineImages);
-		while ($idx > 0) {
-			$idx--;
-			$sBefore = substr($sWholeText, 0, $aInlineImages[$idx]['position']);
-			$sAfter = substr($sWholeText, $aInlineImages[$idx]['position']);
-			$sWholeText = $sBefore." [itop attachment: {$aInlineImages[$idx]['cid']}] ".$sAfter;
-		}
-		echo "=================\n";
-		echo "$sWholeText\n";
-		echo "=================\n";
+			foreach ($aMatches[1] as $idx => $aInfo) {
+				$sCID = $aInfo[0];
+				if (!array_key_exists($sCID, $aCIDToImage)) {
+					echo "ERROR: inline image: $sCID NOT FOUND !!!\n";
+				} else {
+					$aInlineImages[$idx]['cid'] = $sCID;
+					echo "Ok, inline image {$aCIDToImage[$sCID]['filename']} as cid:$sCID\n";
+				}
+			}
+			$sWholeText = $oEmail->sBodyText;
+			$idx = count($aInlineImages);
+			while ($idx > 0) {
+				$idx--;
+				$sBefore = substr($sWholeText, 0, $aInlineImages[$idx]['position']);
+				$sAfter = substr($sWholeText, $aInlineImages[$idx]['position']);
+				$sWholeText = $sBefore . " [itop attachment: {$aInlineImages[$idx]['cid']}] " . $sAfter;
+			}
+			echo "=================\n";
+			echo "$sWholeText\n";
+			echo "=================\n";
 
-		$sBodyText = $oEmail->StripTags($sWholeText);
-	} else {
-		echo "Inline Images: no inline-image found\n";
-		$sBodyText = $oEmail->StripTags();
+			$sBodyText = $oEmail->StripTags($sWholeText);
+		} else {
+			echo "Inline Images: no inline-image found\n";
+			$sBodyText = $oEmail->StripTags();
+		}
+		echo "------ BODY Text version\n";
+		echo "Plain Text Version:\n$sBodyText\n";
 	}
-	echo "------ BODY Text version\n";
-	echo "Plain Text Version:\n$sBodyText\n";
 }
 
-echo "\n\n\n\n\n\n";
-echo "========================================================================================================================\n";
-echo "GetNewPartHTML() returned:\n";
-echo "===============================\n";
-echo $oEmail->GetNewPartHTML($oEmail->sBodyText);
-echo "===============================\n";
-
+if ($bOutputDetails) {
+	echo "\n\n\n\n\n\n";
+	echo "========================================================================================================================\n";
+	echo "GetNewPartHTML() returned:\n";
+	echo "===============================\n";
+	echo $oEmail->GetNewPartHTML($oEmail->sBodyText);
+	echo "===============================\n";
+}
 
 echo "\n\n\n\n\n\n";
 echo "========================================================================================================================\n";
